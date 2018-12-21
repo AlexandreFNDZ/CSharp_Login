f7636467c16676a93c67472c3d909002e69cde36
Alexandre Pereira <alexandre.pereira@webtraining.int>
2018-12-21 13:48:20 -0200

    Projeto de autenticação através do fornecimento de usuario e senha, desenvolvido para fins de aprendizado

---
+++ Login01/FazerLogin.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Login01
+{
+    public class FazerLogin
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public FazerLogin(string username, string password)
+        {
+            Username = "Usuario64";
+            Password = "senha123";
+
+            if (Username != username || Password != password)
+            {
+                throw new ArgumentException("Usuário e/ou Senha inválidos! Confira os dados digitados");
+            }          
+        }     
+    }
+}
---
+++ Login01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Login01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {         
+            string username = "Usuario64";
+            string password = "senha123";
+
+            try
+            {
+                FazerLogin login = new FazerLogin(username, password);
+
+                Console.WriteLine(login.Username);
+                Console.WriteLine(login.Password);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            
+            Console.ReadLine();
+        }
+    }
+}
